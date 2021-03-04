6b9b759c715971d593bdd0e9ce1da4c2a587663e
William <23605383@sun.ac.za>
2021-03-04 23:04:25 +0200

    Little changes in app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jokes = require("everyday-fun");
+
+
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("./index.html");
+})
